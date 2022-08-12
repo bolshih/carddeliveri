76fdcbf7e81caa0a3e0cab65fadaf5816ec44cd4
bolshih <artem.bolshih@mail.ru>
2022-08-12 14:20:09 +0300

    доработанно

--- src/test/java/OrderCardDeliveriTest.java
+++ src/test/java/OrderCardDeliveriTest.java
@@ -4,62 +4,65 @@ import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.conditions.Enabled;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
-import java.text.DateFormat;
+
 import java.time.Duration;
-import java.util.Date;
-import java.util.Locale;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class OrderCardDeliveriTest {
-    Date date = new Date();
-    //Date date2 = new Date();
-    Date currentDate = new Date();
-    DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT, Locale.forLanguageTag("ru"));
 
     @BeforeEach
     public void setup() {
-        int day = currentDate.getDate();
-        day = day + 5;
-        date.setDate(day);
-        System.out.println(df.format(date));
         open("http://localhost:9999");
-        //int day2 = 28;
-        //date2.setDate(day2);
     }
-
+    public String generateDate(int days, String pattern) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
+    }
 
     @Test
     void shuldTestWebUI() {
-        $("[data-test-id=city] input").setValue("Улан-Удэ");
-        $("[data-test-id=date] input").setValue(df.format(currentDate));
-        $("[data-test-id=name] input").setValue("Александр Семенов-Баринов");
+        String meetingData = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='city'] input").setValue("Улан-Удэ");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(meetingData);
+        $("[data-test-id='name'] input").setValue("Александр Семенов-Баринов");
         $("[name=phone]").setValue("+74785847656");
-        $("[data-test-id=agreement]").click();
+        $("[data-test-id='agreement']").click();
         $(".button").click();
-        $("[data-test-id=notification").shouldHave(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id='notification'] .notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + meetingData), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 
 
     @Test
     void shuldTestWebUIComlexElements() {
+        String meetingDay = generateDate(4, "dd");
+        String meetingMonth = generateDate(4, "MM");
+        String meetingData = generateDate(4, "dd.MM.yyyy");
         $("[data-test-id=city] input").setValue("Ул");
         $(".menu-item").shouldBe(Condition.visible);
         $x("//*[contains(text(), 'Улан-Удэ')]").click();
         $("[data-test-id=date] input").click();
+        $(".calendar").shouldBe(Condition.visible);
         SelenideElement calendar = $(".calendar");
-        if (currentDate.getDate() < 28) { //проверка что дата записи не выпадает на следующем месяц
-            calendar.$$(".calendar__day").find(exactText(String.valueOf(date.getDate()))).click();
+        //проверка что дата записи не выпадает на следующем месяц
+        if (LocalDate.now().format(DateTimeFormatter.ofPattern("MM")).equals(meetingMonth)) {
+            calendar.$$(".calendar__day").find(exactText(meetingDay)).click();
         } else {
             calendar.$("[data-step='1']").click();
-            calendar.$$(".calendar__day").find(exactText(String.valueOf(date.getDate()))).click();
+            calendar.$$(".calendar__day").find(exactText(meetingDay)).click();
         }
         $("[data-test-id=name] input").setValue("Александр Семенов-Баринов");
         $("[name=phone]").setValue("+74785847656");
         $("[data-test-id=agreement]").click();
         $(".button").click();
-        $("[data-test-id=notification").shouldHave(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id='notification'] .notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + meetingData), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 }
